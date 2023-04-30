2684599f14032148cfdcd4d9fd1f68aa2b7dda29
jason-gumbs <wilfred.smithandsmith@gmail.com>
2023-04-30 11:53:54 -0400

    update

--- spring-boot-app/src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
+++ spring-boot-app/src/main/java/com/example/dockerize/springbootapp/SpringBootAppApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/jason/api/hello")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
